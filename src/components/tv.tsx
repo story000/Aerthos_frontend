98eccd373274803aeed9a376f6449855e662cabf
story <lsy1538756437@126.com>
2025-02-04 02:04:12 -0500

    small

--- src/components/tv.tsx
+++ src/components/tv.tsx
@@ -24,7 +24,7 @@ export default function ChartComponent() {
       download: true,
       header: true,
       dynamicTyping: true,
-      complete: (result) => {
+      complete: (result: Papa.ParseResult<any>) => {
         const parsedData = result.data as any[];
         
         const formattedData: CandleData[] = parsedData
